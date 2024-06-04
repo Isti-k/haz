560c3dd27c460aa30847b592afc84c81218e4cd0
Isti-k <kiscserkes03@gmail.com>
2024-06-04 10:57:56 +0200

    program

--- main.py
+++ main.py
@@ -5,11 +5,12 @@ from pybricks.parameters import Port, Stop, Direction, Button, Color
 from pybricks.tools import wait, StopWatch, DataLog
 from pybricks.robotics import DriveBase
 from pybricks.media.ev3dev import SoundFile, ImageFile
+import robotPorszivo
 
 
-oraiMunka=robotPorszivo.Palya()
+oraiMunka=robotPorszivo.Feladat()
 
-oraiMunka.robotPorszivo()
+oraiMunka.porszivo()
 
 
 
--- robotPorszivo.py
+++ robotPorszivo.py
@@ -5,7 +5,7 @@ from pybricks.tools import wait, StopWatch, DataLog
 from pybricks.robotics import DriveBase
 from pybricks.media.ev3dev import SoundFile, ImageFile
 
-class Feladatok():
+class Feladat():
 
     def __init__(self):
         # tégla
@@ -15,11 +15,29 @@ class Feladatok():
         self.bm = Motor(Port.C)
         self.km = Motor(Port.A)
         # szenzorok
-        self.cs = ColorSensor(Port.S3)
+        #self.cs = ColorSensor(Port.S3)
         self.ts = TouchSensor(Port.S1)
-        self.gs = GyroSensor(Port.S2)
+        #self.gs = GyroSensor(Port.S2)
         self.us = UltrasonicSensor(Port.S4)
         #self.ir = InfraredSensor(Port.S4)
 
         # dupla motorkezelő
         self.robot = DriveBase(self.jm, self.bm, 55, 90)
+
+
+    def porszivo(self):
+        self.robot.settings(straight_speed=150)
+        self.robot.straight(300)
+        self.robot.turn(120)
+        self.robot.straight(50)
+        self.robot.turn(120)
+        self.robot.straight(300)
+        self.robot.turn(-120)
+        self.robot.straight(50)
+        self.robot.turn(-120)
+        self.robot.straight(300)
+        self.robot.turn(120)
+        self.robot.straight(50)
+        self.robot.turn(120)
+        self.robot.straight(300)
+        self.robot.stop(Stop.BREAK)
